8a51e6633de778c97d2f6f1e3a548fdd2513a808
olegf <olegfat450@gmail.com>
2024-10-29 00:40:30 +0700

    commit

--- app/src/main/java/com/example/timecalculator/MainActivity.kt
+++ app/src/main/java/com/example/timecalculator/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.timecalculator
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+//import android.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbarMain: Toolbar
 
     private lateinit var textResult: TextView
     private lateinit var buttonSum: Button
@@ -24,10 +29,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var s2: EditText
 
 
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+            when(item.itemId) {
+                R.id.reset -> {
+                    h1.text.clear();m1.text.clear();s1.text.clear()
+                    h2.text.clear();m2.text.clear();s2.text.clear()
+                    textResult.text = "Результат"
+                    message("Данные очищены").show()
+                }
+
+                R.id.exit -> {
+                    message("Приложение закрыто").show();finish()
+                }
+            }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "  Калькулятор времени"
+        toolbarMain.subtitle = "                    версия 1.0"
+        toolbarMain.setLogo(R.drawable.logo)
 
         h1 = findViewById(R.id.h1)
         m1 = findViewById(R.id.m1)
@@ -44,6 +78,8 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    fun message(message: String) = Toast.makeText(applicationContext,message,Toast.LENGTH_LONG)
 
     fun operation(v: View) {
         var h1r: Int; var m1r: Int; var s1r: Int
@@ -66,8 +102,9 @@ class MainActivity : AppCompatActivity() {
         val hr: Int = result / 3600
         val mr: Int = (result - hr*3600)/60
         val sr: Int = result - (hr*3600 + mr*60)
-
-        textResult.text = "${hr}h  ${mr}m  ${sr}s"
+         textResult.setTextColor(resources.getColor(R.color.color2))
+         textResult.text = "${hr}h  ${mr}m  ${sr}s"
+        message(textResult.text.toString()).show()
     }
 
 }
